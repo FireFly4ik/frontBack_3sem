72404ee9da9ee6413b197e8d8031638d6a25bbd7
FireFly4ik <andreybatman06@gmail.com>
2026-04-11 14:49:18 +0300

    13pr

---
+++ notes_app/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'notes-cache-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './app.js',
+    'https://unpkg.com/chota@latest'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).catch(() => {
+                    // Fallback to index.html for navigation requests
+                    if (event.request.mode === 'navigate') {
+                        return caches.match('./index.html');
+                    }
+                });
+            })
+    );
+});
